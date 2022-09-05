87f0f6319d11603d5be72edad8f7b086eea7a600
ekaspersen <eskilkasp@gmail.com>
2022-09-05 21:13:26 +0200

    Yes everything

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ['./*.{html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#0F1417',
+        secondary: '#141A1F',
+        'main-text': '#A9B9E6',
+        'input-color': '#202934',
+        gmail: '#FF3B3B',
+        facebook: '#16148E',
+        linkedin: '#3DA2FF',
+        'hover-btn': '#6B8DEE',
+      },
+    },
+  },
+  plugins: [],
+};
